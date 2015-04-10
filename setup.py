a92bed8992b57f7ca40854c8c51384db8ffa270c
Torsten Irländer <torsten.irlaender@intevation.de>
2015-04-10 13:31:09 +0200

    Add future package as dependecy to make the code python 3 compatible.

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['pyparsing'],
+    install_requires=['pyparsing', 'future'],
 
 
     # List additional groups of dependencies here (e.g. development dependencies).
