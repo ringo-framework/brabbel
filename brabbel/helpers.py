94463cdee7a3d8334ba01d1897417bdd44df3b75
Torsten Irländer <torsten@irlaender.de>
2015-04-14 20:35:47 +0200

    Added helper method for division under python3.

--- brabbel/helpers.py
+++ brabbel/helpers.py
@@ -11,6 +11,7 @@ def _in(a, b):
     return a in b
 
 def _div(a, b):
+    """Returns float in all cases"""
     return a / b
 
 ########################################################################
