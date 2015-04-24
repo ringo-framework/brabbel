025fc5aefb415d4d67e368085f92b29b9c14bbff
Torsten Irländer <torsten.irlaender@intevation.de>
2015-04-24 23:38:07 +0200

    Removed __init__ and eval methods.

--- brabbel/operators.py
+++ brabbel/operators.py
@@ -4,35 +4,15 @@ class Operator(object):
     """Baseclass for all operators"""
 
     def eval(self):
-        return False
+        raise NotImplementedError()
 
 class Binary(Operator):
     """Baseclass for Binary operators"""
 
-    def __init__(self):
-        """"""
-        Operator.__init__(self)
-
-    def eval(self, a, b):
-        return False
-
 class Unary(Operator):
     """Baseclass for Unary operators"""
 
-    def __init__(self): 
-        """"""
-        Operator.__init__(self)
-
-    def eval(self, a):
-        return False
-
 class Div(Binary):
-
-    """Docstring for Div. """
-
-    def __init__(self):
-        """TODO: to be defined1. """
-        Binary.__init__(self)
 
     def eval(self, a, b):
         if isinstance(b, int):
