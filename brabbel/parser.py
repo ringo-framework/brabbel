1a7f020785f9278ed2e564cc5a63f5ffca96ffe5
Torsten Irländer <torsten.irlaender@intevation.de>
2015-04-25 00:12:52 +0200

    Convert all strings into unicode

--- brabbel/parser.py
+++ brabbel/parser.py
@@ -22,6 +22,10 @@ ParserElement.enablePackrat()
 #                               Helpers                                #
 ########################################################################
 
+def _str(origString, loc, tokens):
+    return unicode(tokens[0])
+
+
 def _number(origString, loc, tokens):
     try:
         return int(tokens[0])
@@ -55,7 +59,7 @@ number = Combine(Optional("-") + Word(nums + '.')).setParseAction(_number)
 # TODO: Remove "-" from list of allowed chars. Is only here for
 # compatibility. (None) <2014-10-28 14:04>
 variable = Combine("$" + Word(alphanums + "_" + "-" + "."))
-string = Combine(lquote.suppress() + Optional(Word(alphanums + "_" + " " + "-" + ":")) + rquote.suppress())
+string = Combine(lquote.suppress() + Optional(Word(alphanums + "_" + " " + "-" + ":")) + rquote.suppress()).setParseAction(_str)
 identifier = Word(alphas + "_")
 none = Literal("None").setParseAction(lambda t: False)
 true = Literal("True").setParseAction(lambda t: True)
