37a9f18cef2b7dd592acbe76d79a152c099605ef
Torsten Irländer <torsten@irlaender.de>
2015-04-29 20:45:45 +0200

    Fixed test cases

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -4,11 +4,11 @@ import unittest
 class TestInOperator(unittest.TestCase):
 
     def test_ok(self):
-        from brabbel.helpers import _in
-        result = _in(1, [1, 2])
+        from brabbel.operators import In
+        result = In().eval(1, [1, 2])
         self.assertEqual(result, True)
 
     def test_fail(self):
-        from brabbel.helpers import _in
-        result = _in(3, [1, 2])
+        from brabbel.operators import In
+        result = In().eval(3, [1, 2])
         self.assertEqual(result, False)
