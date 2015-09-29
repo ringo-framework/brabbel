c4eca68f03bb557047c9a234bfbbdc03b8ca620c
Torsten Irländer <torsten.irlaender@intevation.de>
2015-09-29 14:18:37 +0200

    set version to 0.5.0 dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.4.3',
+    version='0.5.0',
 
     description='A simple python based expression language',
     long_description=long_description,
